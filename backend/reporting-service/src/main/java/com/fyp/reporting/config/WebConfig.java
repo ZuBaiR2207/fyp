8ea585ecc2ddb816eafd22515fe99b1a1ac8cf6f
ZuBaiR2207 <zjubair2122@gmail.com>
2026-04-20 10:47:11 +0800

    Fixed the CORS url from reporting service

--- backend/reporting-service/src/main/java/com/fyp/reporting/config/WebConfig.java
+++ backend/reporting-service/src/main/java/com/fyp/reporting/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173", "http://localhost:5174", "http://localhost:3000")
+                .allowedOrigins("https://zubair-fyp-frontend.onrender.com","http://localhost:5173")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
